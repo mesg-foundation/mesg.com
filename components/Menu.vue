11a9504b9f1d94e5f858ec6bb6dae74c043825f4
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-06-05 13:01:25 -0700

    update new link for discord

--- components/Menu.vue
+++ components/Menu.vue
@@ -19,7 +19,7 @@ export default {
     menus () {
       return [
         { title: "Docs", href: "https://docs.mesg.tech" },
-        { title: "Discord", href: "https://discord.gg/5tVTHJC" },
+        { title: "Discord", href: "https://discord.gg/SaZ5HcE" },
         { title: "Blog", href: "https://medium.com/mesg" }
       ]
     }
