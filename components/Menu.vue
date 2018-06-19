2a473eb5ac30e9c2812981b6001e56f9afd1cb04
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-06-19 19:06:14 +0700

    add jobs menu

--- components/Menu.vue
+++ components/Menu.vue
@@ -29,6 +29,7 @@ export default {
       return [
         { title: "Docs", href: "https://docs.mesg.com" },
         { title: "Marketplace", href: "/marketplace",  internal: true },
+        { title: "Jobs", href: "https://github.com/mesg-foundation/core/wiki/Jobs" },
         { title: "Discord", href: "https://discord.gg/SaZ5HcE" },
         { title: "Blog", href: "https://medium.com/mesg" }
       ]
