64aa589890188d77f23aadacb1618d9d39e3c8f7
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-06-27 16:03:02 +0700

    add sharing button

--- components/Sharing.vue
+++ components/Sharing.vue
@@ -5,10 +5,13 @@
       <p>By connecting services to Core rather than your application, they can be re-used and shared, effectively transforming services into sharable commodities.</p>
       <p>Core features event-driven architecture, allowing you to experiment with different services, including cross-chain transactions. This allows maximum flexibility to keep iterating your application until your perfect solution is developed. </p>
       <p>That’s just the beginning. Together we're building a decentralized infrastructure so developers can just plug and play and be rewarded for their contributions. </p>
+      <nuxt-link class="button" to="/marketplace">Checkout some existing services</nuxt-link>
     </div>
   </section>
 </template>
 
-<style scopped>
-  p { margin-bottom: 1em; }
+<style scoped>
+  p {
+    margin-bottom: 1.5em;
+  }
 </style>
