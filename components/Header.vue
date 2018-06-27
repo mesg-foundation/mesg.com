0383356ae0052003650b473b6e9132d27c46a9ae
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-06-27 19:38:47 +0700

    update header text

--- components/Header.vue
+++ components/Header.vue
@@ -2,8 +2,8 @@
   <header class="container">
     <h1>Build applications connected to <AnimatedHeader :words="words"></AnimatedHeader></h1>
     <p>
-      MESG is an open-source ultra-powerful building framework for developers which connects all technologies, including blockchains.
-      Instead of connecting services directly to applications, connect them to Core so services can be reused and shared in a decentralized ecosystem.
+      MESG is an open-source technology-agnostic framework for developing service-oriented applications.
+      It’s a building platform where services can be connected to all technologies and be reused and shared so you can focus on innovation rather than implementation.
     </p>
     <p>
       <span class="mono shadow">bash &lt;(curl -s https://mesg.com/install)</span>
