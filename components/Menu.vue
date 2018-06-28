e45eb4f8e21a7651e4dc57ac6c9f70e6e8d76ef9
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-06-28 18:28:45 +0700

    update menu for mobile

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,18 +1,32 @@
 <template>
-  <nav class="container">
+  <nav class="container" :class="{ 'open': menuShow }">
     <nuxt-link to="/" class="title">
       <img src="~/assets/logo.svg" alt="">
     </nuxt-link>
+    <a
+      href="#"
+      class="toggle"
+      @click="menuShow = !menuShow"
+      v-html="menu">
+    </a>
     <div class="nav-items">
+      <a
+        href="#"
+        class="toggle"
+        @click="menuShow = !menuShow">
+        &times;
+      </a>
       <template v-for="menu in menus" >
         <nuxt-link
           v-if="menu.internal"
+          @click.native="menuShow = false"
           :key="menu.href"
           :to="menu.href">
           {{ menu.title }}
         </nuxt-link>
         <a
           v-else
+          @click.native="menuShow = false"
           :key="menu.href"
           :href="menu.href">
           {{ menu.title }}
@@ -24,6 +38,18 @@
 
 <script>
 export default {
+  props: {
+    menu: {
+      type: String,
+      // default: '&#x1f354'
+      default: '&#9776'
+    }
+  },
+  data () {
+    return {
+      menuShow: false
+    }
+  },
   computed: {
     menus () {
       return [
@@ -50,6 +76,10 @@ img {
   height: 2.5em;
   vertical-align: middle;
 }
+.toggle {
+  display: none;
+}
+.toggle:hover, .toggle:active { color: white; }
 .nav-items {
   display: flex;
   align-items: center;
@@ -90,10 +120,46 @@ a:hover { color: #FFCB1B }
     flex-wrap: wrap;
     justify-content: center;
   }
-}
-@media only screen and (max-width: 580px) {
-  .nav-items a {
-    display: none;
+
+  .toggle {
+    display: block;
+    position: absolute;
+    top: 0;
+    left: 0;
+    font-size: 1.75em;
+    padding: 0;
+    color: white;
+    width: 60px;
+    margin: 0;
+    height: 60px;
+    text-align: center;
+    line-height: 60px;
+    font-weight: lighter;
+  }
+  .nav-items {
+    padding-top: 3em;
+    position: fixed;
+    background: black;
+    height: 100%;
+    top: 0;
+    left: 0;
+    justify-content: left;
+    flex-direction: column;
+    width: 80%;
+    box-shadow: 0 15px 35px rgba(50,50,93,.1), 0 5px 15px rgba(0,0,0,.07);
+    transition: all 300ms;
+    transform: translateX(-100%);
+    border-right: solid 1px rgba(255,255,255,.15);
+  }
+
+  .open .nav-items {
+    transform: translateX(0);
+  }
+
+  .nav-items a:not(.toggle) {
+    width: 100%;
+    padding: 1em 2em;
+    margin: 0;
   }
 }
 </style>
