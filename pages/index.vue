a4532fd87b0da7e6fea41444d1fd8d840a999dd5
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-06-29 11:42:56 +0700

    remove mission page

---
+++ pages/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <Header class="dark"></Header>
+    <GetStarted class="light"></GetStarted>
+    <Sharing class="gray border"></Sharing>
+    <Features class="light border"></Features>
+    <Github class="gray border"></Github>
+    <Roadmap class="light border"></Roadmap>    
+  </div>
+</template>
+
+<script>
+import Header from '~/components/Header'
+import GetStarted from '~/components/GetStarted'
+import Features from '~/components/Features'
+import Github from '~/components/Github'
+import Roadmap from '~/components/Roadmap'
+import Sharing from '~/components/Sharing'
+export default {
+  components: {
+    Header,
+    GetStarted,
+    Features,
+    Github,
+    Roadmap,
+    Sharing
+  }
+}
+</script>
+
+<style scoped>
+  section {
+    padding: 60px 0;
+    text-align: center;
+  }
+
+  .border {
+    border-top: solid 1px #DDD;
+  }
+</style>
+
