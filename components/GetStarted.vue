010a063fbf054c8c29392d7053abd6445834d88c
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-07-02 19:06:06 +0700

    update get started

--- components/GetStarted.vue
+++ components/GetStarted.vue
@@ -2,7 +2,7 @@
   <section>
     <div class="container">
       <h2>Get Started</h2>
-      <p>Create your first application in less than 5 minutes.</p>
+      <p>Create an entire application in less than 5 minutes.</p>
       <Terminal class="shadow" :commands="basicApp" @finished="onFinished"></Terminal>
     </div>
   </section>
