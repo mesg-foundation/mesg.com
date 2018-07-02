ff2de23cb907773a4b407c1c332bfbc3e147d097
Nicolas Mahe <nicolas@mahe.me>
2018-07-02 17:32:20 +0700

    Add schema

--- components/Sharing.vue
+++ components/Sharing.vue
@@ -4,7 +4,8 @@
       <h2>Share and Reuse Services</h2>
       <p>By connecting services to Core rather than your application, they can be re-used and shared, effectively transforming services into sharable commodities.</p>
       <p>Core features event-driven architecture, allowing you to experiment with different services, including cross-chain transactions. This allows maximum flexibility to keep iterating your application until your perfect solution is developed. </p>
-      <p class="no-border">That’s just the beginning. Together we're building a decentralized infrastructure so developers can just plug and play and be rewarded for their contributions. </p>
+      <p class="no-margin">That’s just the beginning. Together we're building a decentralized infrastructure so developers can just plug and play and be rewarded for their contributions. </p>
+      <img class="" src="~/assets/schema1.svg" alt=""/>
       <nuxt-link class="button hover button--success" to="/marketplace">Check out existing services →</nuxt-link>
     </div>
   </section>
@@ -19,10 +20,11 @@
     text-align: justify;
     margin-bottom: 1.5em;
   }
-  .no-border {
-    margin-bottom: 0px
-  }
   a {
     margin-top: 40px;
+  }
+  img {
+    margin-top: 20px;
+    width: 100%;
   }
 </style>
