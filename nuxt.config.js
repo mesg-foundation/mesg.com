6289de19ce92bf4884341300334e1114d5078980
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-08-19 21:05:26 +0700

    disable curl

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,9 +47,9 @@ module.exports = {
       id: 'UA-117583862-2'
     }]
   ],
-  router: {
-    middleware: ['mesg-curl']
-  },
+  // router: {
+  //   middleware: ['mesg-curl']
+  // },
   /*
   ** Build configuration
   */
