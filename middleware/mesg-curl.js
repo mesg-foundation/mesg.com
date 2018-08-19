06c1407d93a98b4491f0608e554dce6da963dbf4
Anthony ESTEBE <anthony.estebe@gmail.com>
2018-08-19 21:32:34 +0700

    fix build issue

--- middleware/mesg-curl.js
+++ middleware/mesg-curl.js
@@ -16,6 +16,7 @@ HAPPY CODING :)
 `
 
 export default function ({ req, res }) {
+  if (!req) { return } // Hack for build
   if (!process.server) { return }
   if (req.headers['user-agent'].startsWith('curl/')) {
     return res.end(message)
