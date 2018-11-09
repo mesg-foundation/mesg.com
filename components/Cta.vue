45ec196918f0b09b34caf3b9ce07f9c744813e03
Emmanuel <drouinemmanuel@gmail.com>
2018-11-09 12:40:01 +0700

    Update CTA section with new structure

--- components/Cta.vue
+++ components/Cta.vue
@@ -1,31 +1,32 @@
 <template>
   <section>
-  <div class="container-parent">
-      <div class="container-child" flex row space-between align-center>
-        <div half flex column justify-center align-center>
-        <h2>Start to build now</h2>
-        <p mb2>
-          MESG is community-driven. Take the reigns: building is in your hands.
-          To get started, open your terminal and run:
-
-        </p>
-          <nav flex>
-            <Button href="https://docs.mesg.com/" target="_blank" purple><i class="fas fa-code"></i> Developers</Button>
+    <div class="container-parent" flex row column-reverse-responsive justify-center align-center>
+      <div class="container-content" half flex column justify-center align-center>
+          <h2>Start to build now</h2>
+          <p mb2>
+            MESG is community-driven. Take the reigns: building is in your hands.<br>
+            To get started, open your terminal and run:
+          </p>
+          <nav flex justify-center align-center>
+            <Button href="https://docs.mesg.com/" target="_blank" primary><i class="fas fa-code"></i> Developers</Button>
           </nav>
         </div>
-        <div class="line-separator" flex column justify-center align-center>
+        <div class="fond-1" half flex column justify-center align-center>
         </div>
-        <div half flex column justify-center align-center>
-        <h2>Start optimizing today</h2>
-        <p mb2>
-          Find out how you can start automating, streamlining and expanding your business today with MESG.
-        </p>
+      </div>
+    <div class="container-parent" flex row justify-center align-center>
+      <div class="fond-2" half flex column justify-center align-center>
+      </div>
+      <div class="container-content" half flex column justify-center align-center>
+          <h2>Start optimizing today</h2>
+          <p mb2>
+            Find out how you can start automating, streamlining and expanding your business today with MESG.
+          </p>
           <nav flex>
             <Button href="https://goo.gl/forms/ifXyNfaQn8VC7pfu1" target="_blank" primary><i class="fas fa-chart-line"></i> Business</Button>
           </nav>
-        </div>
       </div>
-  </div>
+    </div>
   </section>
 </template>
 
@@ -51,32 +52,64 @@ i {
   margin-right: 0.3em;
 }
 
-.line-separator {
-  height: 245px;
+.fond-1 {
+  background-image:linear-gradient(296deg, rgba(148, 82, 255, 0.75) 0%, rgba(191, 155, 249, 0.75) 100%), url("~/assets/moon.jpg");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size:cover;
+  height: 350px;
   max-height: 100%;
-  border-style: solid;
-  border-width: 1px;
-  border-image-source: linear-gradient(to top, #9452ff 0%, #30b24b 100%);
-  border-image-source: linear-gradient(to top, var(--lighter-purple) 0%, var(--green-cta) 100%);
-  border-image-slice: 1;
+  width: 50%;
+  max-width: 720px;
+  padding:0;
 }
+.fond-2 {
+  background-image: linear-gradient(296deg, rgba(191, 155, 249, 0.75) 0%, rgba(148, 82, 255, 0.75) 100%), url("~/assets/building.jpg");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size:cover;
+  height: 350px;
+  max-height: 100%;
+  width: 50%;
+  max-width: 720px;
+  padding:0;
+}
+.container-content {
+  padding: 2.4em;
+  width: 50%;
+  max-width: 720px;
+}
+.container-parent {
+  padding:0;
+  height: 100%;
+}
+
+
 @media only screen and (max-width: 768px) {
-  .line-separator {
+  .fond-1 {
+    width:100%;
+    max-width: 768px;
+    height: 280px;
+  }
+  .fond-2 {
     width: 100%;
-    max-width: 100%;
-    height: 1px;
-    margin: 0;
-    margin-top: 2.4em;
-    margin-bottom: 2.4em;
-    border-image-source: linear-gradient(to right, #9452ff 0%, #30b24b 100%);
-    border-image-source: linear-gradient(to right, var(--lighter-purple) 0%, var(--green-cta) 100%);
-    border-image-slice: 1;
+    max-width: 768px;
+    height: 280px;
+  }
+  .container-content {
+    width: 100%;
+    max-width: 768px;
   }
 }
 @media only screen and (max-width: 414px) {
-  .line-separator {
-    margin-top: 1.6em;
-    margin-bottom: 1.6em;
+  .fond-1 {
+    height: 200px;
+  }
+  .fond-2 {
+    height: 200px;
+  }
+  .container-content {
+    padding: 1.8em;
   }
 }
 </style>
