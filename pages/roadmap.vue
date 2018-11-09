d43e768c4b46b7549ce11e1df059bad6091a259b
Emmanuel <drouinemmanuel@gmail.com>
2018-11-09 16:29:55 +0700

    Add Pages Team + Roadmap

---
+++ pages/roadmap.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <HeaderTeam id="introduction" class="dark"/>
+    <Roadmap id="roadmap" class="dark"/>
+    <Partners id="partners" class="white"/>
+  </div>
+</template>
+
+<script>
+import HeaderTeam from '~/components/HeaderTeam'
+import Roadmap from '~/components/Roadmap'
+import Partners from '~/components/Partners'
+export default {
+  components: {
+    HeaderTeam,
+    Roadmap,
+    Partners
+  }
+}
+</script>
---
+++ pages/team.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <HeaderTeam id="introduction" class="dark"/>
+    <Team id="team" class="white"/>
+    <CtaHiring id="ctahiring" class="dark"/>
+    <Partners id="partners" class="white"/>
+  </div>
+</template>
+
+<script>
+import HeaderTeam from '~/components/HeaderTeam'
+import Team from '~/components/Team'
+import CtaHiring from '~/components/CtaHiring'
+import Partners from '~/components/Partners'
+export default {
+  components: {
+    HeaderTeam,
+    Team,
+    CtaHiring,
+    Partners
+  }
+}
+</script>
