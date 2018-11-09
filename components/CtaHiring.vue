25423ef33fbd098f0378fc0a5ab4bc626e9268aa
Emmanuel <drouinemmanuel@gmail.com>
2018-11-09 16:28:12 +0700

    Add CTA Hiring

---
+++ components/CtaHiring.vue
@@ -0,0 +1,40 @@
+<template>
+  <section>
+  <div class="container-parent">
+    <div class="container-child" flex row justify-center align-center>
+      <div class="content" flex column justify-center align-center>
+        <h2>
+          Want to work on a great project?
+        </h2>
+        <p mb2>Because we are always searching for new talents, feel free to send us your resume.</p>
+        <nav flex>
+          <Button href="https://goo.gl/forms/ifXyNfaQn8VC7pfu1" target="_blank" primary>We are Hiring</Button>
+        </nav>
+      </div>
+    </div>
+  </div>
+  </section>
+</template>
+
+<script>
+import Button from '~/components/Button'
+export default {
+  components: {
+    Button
+  }
+}
+</script>
+
+<style scoped>
+
+.container-parent {
+  background-image:linear-gradient(155deg, rgba(37, 20, 64, 0.9) 0%, rgba(73, 30, 140, 0.9) 100%), url("~/assets/Hire.jpg");
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size:cover;
+  text-align: center;
+}
+
+
+
+</style>
