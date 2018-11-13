c32795035cb4a668bc14810914328df664a80c7c
Emmanuel <drouinemmanuel@gmail.com>
2018-11-13 18:01:30 +0700

    Roadmap responsive

--- components/Roadmap.vue
+++ components/Roadmap.vue
@@ -2,30 +2,34 @@
   <section>
     <div class="container-parent">
       <div class="container-child" flex column align-center>
-        <ul class="timeline" flex column justify-center>
-          <li v-for="(item, i) in roadmap" :key="i">
-            <div flex row align-top>
-              <span flex column class="timeline-date">{{ item.releasedAt }}
-              </span>
-              <p class="Q" flex column justify-center align-center>{{ item.Q }}
-              </p>
-              <div class="timeline-content" flex column>
-                <p class="user" mb1>{{ item.user }}</p>
-                <h4 mb1>{{ item.title }}</h4>
-                <p>{{ item.b1 }}</p>
-                <p>{{ item.b2 }}</p>
-                <p>{{ item.b3 }}</p>
-              </div>
+        <ul class="timeline-years" flex column justify-center>
+          <li v-for="year in roadmap" :key="year">
+            <div class="year" flex column justify-center align-right>
+              <h2 mr2>{{year.year}}</h2>
             </div>
+            <ul class="quarters" flex column justify-center>
+              <li v-for="quarter in year.quarters" :key="quarter">
+                <p class="Q" flex column justify-center align-center mb05><strong>{{ quarter.name }}</strong></p>
+                <div class="timeline" flex column>
+                  <div class="content" mb05>
+                    <p class="user" flex column align-center>{{ quarter.users }}</p>
+                    <div v-for="goal in quarter.goals" :key="goal">
+                      <h4 mb1 mt1>{{ goal.title }}</h4>
+                      <p v-for="sub in goal.subgoals" :key="sub" mb05>{{ sub }}</p>
+                    </div>
+                  </div>
+                </div>
+              </li>
+            </ul>
           </li>
-          <div flex column class="timeline">
-            <div class="arrival">
-              <h4 mb1 class="titlefinal">MESG Enterprise Solutions</h4>
-              <p class="final">A profit-driven subsidiary of the MESG Foundation</p>
-              <p class="final">Delivering the global standard for the enterprise adoption of decentralized technology</p>
-            </div>
-          </div>
         </ul>
+        <div class="solutions" flex column justify-center>
+          <div class="arrival">
+            <h4 mb1 class="titlefinal">MESG Enterprise Solutions</h4>
+            <p class="final" mb05>A profit-driven subsidiary of the MESG Foundation</p>
+            <p class="final">Delivering the global standard for the enterprise adoption of decentralized technology</p>
+          </div>
+        </div>
       </div>
     </div>
   </section>
@@ -41,18 +45,50 @@ export default {
 </script>
 
 <style scoped>
-ul {
-  list-style: none;
-  padding: 0;
+
+ul{
   width: 100%;
-  max-width: 720px;
-  overflow: hidden;
+  max-width: 600px;
+  list-style: none;
+}
+
+h2 {
+  color:#9452ff;
+  color:var(--lighter-purple);
+  margin-bottom: 0;
+}
+h4 {
+  color:#ffffff;
+  color:var(--white-content);
+}
+
+.container-parent {
+  padding-top:0;
+}
+.container-child {
+  padding-top:0;
+}
+.content {
+  position: relative;
+  margin-top:-64px;
+  margin-left:1.2em;
+}
+.quarters {
+  width: 85%;
+  max-width: 600px;
+  float: right;
+}
+.year {
+  width: 10%;
+  max-width: 600px;
+  height: 60px;
+  float: left;
   position: relative;
 }
 
 .Q {
   opacity:1;
-  font-family: Quicksand;
+  font-family: QuickSand;
   font-style: medium;
   font-size: 1.6em;
   color:#ffa744;
@@ -62,35 +98,16 @@ ul {
   background-color: #ffffff;
   background-color: var(--white-content);
   border-radius: 50%;
+  position:relative;
 }
 
-.timeline-date {
-  font-family: Quicksand;
-  font-style: medium;
-  font-stretch: normal;
-  line-height: normal;
-  letter-spacing: normal;
-  font-weight: normal;
-  font-size: 1.6em;
-  color:#9452ff;
-  color:var(--lighter-purple);
-  width: 30%;
-  float: left;
-  text-align: right;
-  padding-right: 2.4em;
-}
-
-.timeline-content {
-  width: 60%;
-  float:right;
-  border-left: 1px #9452ff solid;
-  padding-left: 2.4em;
-  padding-bottom: 3.6em;
+.solutions {
+  width: 100%;
+  max-width: 720px;
 }
 .arrival {
   border-radius: 1em;
   padding:2.4em;
-  width: 100%;
   background-color: #ffffff;
   background-color: var(--white-content);
 }
@@ -102,75 +119,83 @@ ul {
   color:#57577e;
   color: var(--dark-grey);
 }
-.user{
+.user {
   opacity:1;
   background-color: #ffa744;
   background-color: var(--Orange-cta);
   border-radius: 3em;
   padding: 0.5em 0.5em;
-  width: 60%;
+  width: 55%;
 }
 
-h4 {
-  color:#ffffff;
-  color:var(--white-content);
+
+.timeline {
+  padding-left: 2.4em;
+  padding-right: 1.8em;
+  left: 1.8em;
+  border-left: dotted 2px #9452ff;
+  position: relative;
+  margin-bottom: 1.2em;
 }
 
-@media only screen and (max-width: 414px) {
-  .timeline-date {
-    padding-right: 1.2em;
+.timeline:before {
+  content: "";
+  width: 15px;
+  height: 15px;
+  left:-9px;
+  top:0;
+  border-radius: 100%;
+  border: solid 2px #9452ff;
+  position: absolute;
+  background: #251440;
+}
+
+.timeline:after {
+  content: "";
+  width: 15px;
+  height: 15px;
+  left:-9px;
+  bottom:0px;
+  border-radius: 100%;
+  border: solid 2px #9452ff;
+  position: absolute;
+  background: #9452ff;
+}
+@media screen and (max-width: 768px) {
+  .timeline {
+    padding-right: 1.8em;
   }
-  .timeline-content {
-    padding-left: 1.2em;
+  .user {
+    width: 60%;
+  }
+}
+@media screen and (max-width: 639px) {
+  .year {
+    width: 100%;
+    height: auto;
+    padding-bottom: 1.2em;
+  }
+  .quarters {
     width: 100%;
   }
-  .arrival {
-    padding:1.2em;
+  .user {
+    width: 70%;
+  }
+}
+@media screen and (max-width: 535px) {
+  .user {
+    width: 100%;
+  }
+}
+@media screen and (max-width: 414px) {
+  .timeline {
+    padding-right: 1.8em;
   }
   .user {
     width: 100%;
   }
 }
 
-/*
-@media screen and (min-width: 840px) {
-  .timeline > li {
-    overflow: hidden;
-    margin: 0;
-    position: relative;
-  }
-  .timeline-date {
-		width: 100px;
-    float: left;
-		text-align: right;
-    padding-right: 20px;
-  }
-  .timeline-content {
-		width: 75%;
-		width: calc(100% - 100px);
-    float: left;
-    border-left: 3px rgba(32, 19, 81, .2) solid;
-		padding-left: 30px;
-	}
-  .timeline-content:before {
-		content: '';
-    width: 12px;
-    height: 12px;
-    background: rgba(32, 19, 81, 1);
-    position: absolute;
-    left: 95px;
-    top: 8px;
-    border-radius: 100%;
-	}
-	.timeline-content h4, .timeline-content p {
-		text-align: justify;
-    margin: 0;
-	}
-  .timeline-content h4 {
-    margin-bottom: .5em;
-  }
-  .timeline-content p {
-    margin-bottom: 1.5em;
-  }
-}*/
+
+
 </style>
