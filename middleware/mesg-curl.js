32bdf6ae0ead44f5876ba0872608f51267ff4c79
Nicolas Mahe <nicolas@mahe.me>
2018-12-23 18:24:37 +0700

    Add curl middleware to redirect curl user-agent to the simple html page

---
+++ middleware/mesg-curl.js
@@ -0,0 +1,26 @@
+const message = `
+███╗   ███╗███████╗███████╗ ██████╗ 
+████╗ ████║██╔════╝██╔════╝██╔════╝ 
+██╔████╔██║█████╗  ███████╗██║  ███╗
+██║╚██╔╝██║██╔══╝  ╚════██║██║   ██║
+██║ ╚═╝ ██║███████╗███████║╚██████╔╝
+╚═╝     ╚═╝╚══════╝╚══════╝ ╚═════╝ 
+
+Website: https://mesg.com
+Forum: https://forum.mesg.com
+Documentation: https://docs.mesg.com
+Github: https://github.com/mesg-foundation
+
+Install the core by executing:
+bash <(curl -s https://mesg.com/install)
+
+HAPPY CODING :)
+`
+
+export default function ({ req, res }) {
+  if (!req) { return } // Hack for build
+  if (!process.server) { return }
+  if (req.headers['user-agent'].startsWith('curl/')) {
+    return res.end(message)
+  }
+}
