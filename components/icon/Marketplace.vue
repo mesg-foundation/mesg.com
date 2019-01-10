58a25a73c6b87fca0fc397c15d67cd0914deb9dc
Emmanuel Drouin <emmanuel@mesg.tech>
2019-01-10 16:04:02 +0700

    Update: Schemas, Icons, Purple Background > Header and CTA.

--- components/icon/Marketplace.vue
+++ components/icon/Marketplace.vue
@@ -3,117 +3,117 @@
   	 viewBox="0 0 400 400" style="enable-background:new 0 0 400 400;" xml:space="preserve">
 
   <g>
-  	<linearGradient id="gradient-1" gradientUnits="userSpaceOnUse" x1="95.3262" y1="71.136" x2="4.6738" y2="28.864">
+  	<linearGradient id="gradient1" gradientUnits="userSpaceOnUse" x1="95.3262" y1="71.136" x2="4.6738" y2="28.864">
   		<stop  offset="0" style="stop-color:#491E8C"/>
   		<stop  offset="1" style="stop-color:#9452FF"/>
   	</linearGradient>
   	<circle class="st0" cx="50" cy="50" r="50"/>
   	<circle class="st1" cx="51" cy="51" r="25"/>
-  	<linearGradient id="gradient-2" gradientUnits="userSpaceOnUse" x1="72.6631" y1="60.568" x2="27.3369" y2="39.432">
+  	<linearGradient id="gradient2" gradientUnits="userSpaceOnUse" x1="72.6631" y1="60.568" x2="27.3369" y2="39.432">
   		<stop  offset="0" style="stop-color:#491E8C"/>
   		<stop  offset="1" style="stop-color:#9452FF"/>
   	</linearGradient>
   	<circle class="st2" cx="50" cy="50" r="25"/>
   </g>
   <g>
-  	<linearGradient id="gradient-3" gradientUnits="userSpaceOnUse" x1="245.3262" y1="71.136" x2="154.6738" y2="28.864">
+  	<linearGradient id="gradient3" gradientUnits="userSpaceOnUse" x1="245.3262" y1="71.136" x2="154.6738" y2="28.864">
   		<stop  offset="0" style="stop-color:#491E8C"/>
   		<stop  offset="1" style="stop-color:#9452FF"/>
   	</linearGradient>
   	<circle class="st3" cx="200" cy="50" r="50"/>
   	<circle class="st1" cx="201" cy="51" r="25"/>
-  	<linearGradient id="gradient-4" gradientUnits="userSpaceOnUse" x1="222.6631" y1="60.568" x2="177.3369" y2="39.432">
+  	<linearGradient id="gradient4" gradientUnits="userSpaceOnUse" x1="222.6631" y1="60.568" x2="177.3369" y2="39.432">
   		<stop  offset="0" style="stop-color:#491E8C"/>
   		<stop  offset="1" style="stop-color:#9452FF"/>
   	</linearGradient>
   	<circle class="st4" cx="200" cy="50" r="25"/>
   </g>
   <g>
-  	<linearGradient id="gradient-5" gradientUnits="userSpaceOnUse" x1="395.3262" y1="71.136" x2="304.6738" y2="28.864">
+  	<linearGradient id="gradient5" gradientUnits="userSpaceOnUse" x1="395.3262" y1="71.136" x2="304.6738" y2="28.864">
   		<stop  offset="0" style="stop-color:#491E8C"/>
   		<stop  offset="1" style="stop-color:#9452FF"/>
   	</linearGradient>
   	<circle class="st5" cx="350" cy="50" r="50"/>
   	<circle class="st1" cx="351" cy="51" r="25"/>
-  	<linearGradient id="gradient-6" gradientUnits="userSpaceOnUse" x1="372.6631" y1="60.568" x2="327.3369" y2="39.432">
+  	<linearGradient id="gradient6" gradientUnits="userSpaceOnUse" x1="372.6631" y1="60.568" x2="327.3369" y2="39.432">
   		<stop  offset="0" style="stop-color:#491E8C"/>
   		<stop  offset="1" style="stop-color:#9452FF"/>
   	</linearGradient>
   	<circle class="st6" cx="350" cy="50" r="25"/>
   </g>
   <g>
-  	<linearGradient id="gradient-7" gradientUnits="userSpaceOnUse" x1="95.3262" y1="221.136" x2="4.6738" y2="178.864">
+  	<linearGradient id="gradient7" gradientUnits="userSpaceOnUse" x1="95.3262" y1="221.136" x2="4.6738" y2="178.864">
   		<stop  offset="0" style="stop-color:#491E8C"/>
   		<stop  offset="1" style="stop-color:#9452FF"/>
   	</linearGradient>
   	<circle class="st7" cx="50" cy="200" r="50"/>
   	<circle class="st1" cx="51" cy="201" r="25"/>
-  	<linearGradient id="gradient-8" gradientUnits="userSpaceOnUse" x1="72.6631" y1="210.568" x2="27.3369" y2="189.432">
+  	<linearGradient id="gradient8" gradientUnits="userSpaceOnUse" x1="72.6631" y1="210.568" x2="27.3369" y2="189.432">
   		<stop  offset="0" style="stop-color:#491E8C"/>
   		<stop  offset="1" style="stop-color:#9452FF"/>
   	</linearGradient>
   	<circle class="st8" cx="50" cy="200" r="25"/>
   </g>
   <g>
-  	<linearGradient id="gradient-9" gradientUnits="userSpaceOnUse" x1="245.3262" y1="221.136" x2="154.6738" y2="178.864">
+  	<linearGradient id="gradient9" gradientUnits="userSpaceOnUse" x1="245.3262" y1="221.136" x2="154.6738" y2="178.864">
   		<stop  offset="0" style="stop-color:#491E8C"/>
   		<stop  offset="1" style="stop-color:#9452FF"/>
   	</linearGradient>
   	<circle class="st9" cx="200" cy="200" r="50"/>
   	<circle class="st1" cx="201" cy="201" r="25"/>
-  	<linearGradient id="gradient-10" gradientUnits="userSpaceOnUse" x1="222.6631" y1="210.568" x2="177.3369" y2="189.432">
+  	<linearGradient id="gradient10" gradientUnits="userSpaceOnUse" x1="222.6631" y1="210.568" x2="177.3369" y2="189.432">
   		<stop  offset="0" style="stop-color:#491E8C"/>
   		<stop  offset="1" style="stop-color:#9452FF"/>
   	</linearGradient>
   	<circle class="st10" cx="200" cy="200" r="25"/>
   </g>
   <g>
-  	<linearGradient id="gradient-11" gradientUnits="userSpaceOnUse" x1="395.3262" y1="221.136" x2="304.6738" y2="178.864">
+  	<linearGradient id="gradient11" gradientUnits="userSpaceOnUse" x1="395.3262" y1="221.136" x2="304.6738" y2="178.864">
   		<stop  offset="0" style="stop-color:#491E8C"/>
   		<stop  offset="1" style="stop-color:#9452FF"/>
   	</linearGradient>
   	<circle class="st11" cx="350" cy="200" r="50"/>
   	<circle class="st1" cx="351" cy="201" r="25"/>
-  	<linearGradient id="gradient-12" gradientUnits="userSpaceOnUse" x1="372.6631" y1="210.568" x2="327.3369" y2="189.432">
+  	<linearGradient id="gradient12" gradientUnits="userSpaceOnUse" x1="372.6631" y1="210.568" x2="327.3369" y2="189.432">
   		<stop  offset="0" style="stop-color:#491E8C"/>
   		<stop  offset="1" style="stop-color:#9452FF"/>
   	</linearGradient>
   	<circle class="st12" cx="350" cy="200" r="25"/>
   </g>
   <g>
-  	<linearGradient id="gradient-13" gradientUnits="userSpaceOnUse" x1="95.3262" y1="371.136" x2="4.6738" y2="328.864">
+  	<linearGradient id="gradient13" gradientUnits="userSpaceOnUse" x1="95.3262" y1="371.136" x2="4.6738" y2="328.864">
   		<stop  offset="0" style="stop-color:#491E8C"/>
   		<stop  offset="1" style="stop-color:#9452FF"/>
   	</linearGradient>
   	<circle class="st13" cx="50" cy="350" r="50"/>
   	<circle class="st1" cx="51" cy="351" r="25"/>
-  	<linearGradient id="gradient-14" gradientUnits="userSpaceOnUse" x1="72.6631" y1="360.568" x2="27.3369" y2="339.432">
+  	<linearGradient id="gradient14" gradientUnits="userSpaceOnUse" x1="72.6631" y1="360.568" x2="27.3369" y2="339.432">
   		<stop  offset="0" style="stop-color:#491E8C"/>
   		<stop  offset="1" style="stop-color:#9452FF"/>
   	</linearGradient>
   	<circle class="st14" cx="50" cy="350" r="25"/>
   </g>
   <g>
-  	<linearGradient id="gradient-15" gradientUnits="userSpaceOnUse" x1="245.3262" y1="371.136" x2="154.6738" y2="328.864">
+  	<linearGradient id="gradient15" gradientUnits="userSpaceOnUse" x1="245.3262" y1="371.136" x2="154.6738" y2="328.864">
   		<stop  offset="0" style="stop-color:#491E8C"/>
   		<stop  offset="1" style="stop-color:#9452FF"/>
   	</linearGradient>
   	<circle class="st15" cx="200" cy="350" r="50"/>
   	<circle class="st1" cx="201" cy="351" r="25"/>
-  	<linearGradient id="gradient-16" gradientUnits="userSpaceOnUse" x1="222.6631" y1="360.568" x2="177.3369" y2="339.432">
+  	<linearGradient id="gradient16" gradientUnits="userSpaceOnUse" x1="222.6631" y1="360.568" x2="177.3369" y2="339.432">
   		<stop  offset="0" style="stop-color:#491E8C"/>
   		<stop  offset="1" style="stop-color:#9452FF"/>
   	</linearGradient>
   	<circle class="st16" cx="200" cy="350" r="25"/>
   </g>
   <g>
-  	<linearGradient id="gradient-17" gradientUnits="userSpaceOnUse" x1="395.3262" y1="371.136" x2="304.6738" y2="328.864">
+  	<linearGradient id="gradient17" gradientUnits="userSpaceOnUse" x1="395.3262" y1="371.136" x2="304.6738" y2="328.864">
   		<stop  offset="0" style="stop-color:#491E8C"/>
   		<stop  offset="1" style="stop-color:#9452FF"/>
   	</linearGradient>
   	<circle class="st17" cx="350" cy="350" r="50"/>
   	<circle class="st1" cx="351" cy="351" r="25"/>
-  	<linearGradient id="gradient-18" gradientUnits="userSpaceOnUse" x1="372.6631" y1="360.568" x2="327.3369" y2="339.432">
+  	<linearGradient id="gradient18" gradientUnits="userSpaceOnUse" x1="372.6631" y1="360.568" x2="327.3369" y2="339.432">
   		<stop  offset="0" style="stop-color:#491E8C"/>
   		<stop  offset="1" style="stop-color:#9452FF"/>
   	</linearGradient>
@@ -123,23 +123,23 @@
 </template>
 
 <style scoped>
-.st0{opacity:0.45;fill:url(#gradient-1);}
+.st0{opacity:0.45;fill:url(#gradient1);}
 .st1{fill:#1D073F;}
-.st2{fill:url(#gradient-2);}
-.st3{opacity:0.45;fill:url(#gradient-3);}
-.st4{fill:url(#gradient-4);}
-.st5{opacity:0.45;fill:url(#gradient-5);}
-.st6{fill:url(#gradient-6);}
-.st7{opacity:0.45;fill:url(#gradient-7);}
-.st8{fill:url(#gradient-8);}
-.st9{opacity:0.45;fill:url(#gradient-9);}
-.st10{fill:url(#gradient-10);}
-.st11{opacity:0.45;fill:url(#gradient-11);}
-.st12{fill:url(#gradient-12);}
-.st13{opacity:0.45;fill:url(#gradient-13);}
-.st14{fill:url(#gradient-14);}
-.st15{opacity:0.45;fill:url(#gradient-15);}
-.st16{fill:url(#gradient-16);}
-.st17{opacity:0.45;fill:url(#gradient-17);}
-.st18{fill:url(#gradient-18);}
+.st2{fill:url(#gradient2);}
+.st3{opacity:0.45;fill:url(#gradient3);}
+.st4{fill:url(#gradient4);}
+.st5{opacity:0.45;fill:url(#gradient5);}
+.st6{fill:url(#gradient6);}
+.st7{opacity:0.45;fill:url(#gradient7);}
+.st8{fill:url(#gradient8);}
+.st9{opacity:0.45;fill:url(#gradient9);}
+.st10{fill:url(#gradient10);}
+.st11{opacity:0.45;fill:url(#gradient11);}
+.st12{fill:url(#gradient12);}
+.st13{opacity:0.45;fill:url(#gradient13);}
+.st14{fill:url(#gradient14);}
+.st15{opacity:0.45;fill:url(#gradient15);}
+.st16{fill:url(#gradient16);}
+.st17{opacity:0.45;fill:url(#gradient17);}
+.st18{fill:url(#gradient18);}
 </style>
