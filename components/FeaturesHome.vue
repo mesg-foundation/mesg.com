c501b938d266fb41a4bd6bda6bf7c78abd07aa77
İlker Göktuğ Öztürk <ilkergoktugozturk@gmail.com>
2019-03-22 16:14:47 +0300

    created mini-card component & refactored features-home component
    * created & refactored a new card component called mini-card from feature items of features-home component.
    * refactored features-home component and moved its data to store.
    
    resolves a part of #99.

--- components/FeaturesHome.vue
+++ components/FeaturesHome.vue
@@ -7,12 +7,11 @@
           <div class="separator-orange" mb2></div>
         </div>
         <nav flex row justify-center align-center>
-          <nuxt-link v-for="feature in featuresicon" :key="feature.id" :id="feature.id" :to="feature.link" class="entry">
-            <div flex column justify-center align-center>
-              <img :src="feature.icon" :alt="feature.title" class="icon">
-              <p mt1>{{feature.title}}</p>
-            </div>
-          </nuxt-link>
+          <MiniCard v-for="feature in dotFeatures" :key="feature.id"
+            :title="feature.title"
+            :thumbnail="feature.icon"
+            :actionLink="feature.link"
+          />
         </nav>
       </div>
     </div>
@@ -20,93 +19,20 @@
 </template>
 
 <script>
-const featuresicon = [
-  {
-    id: "rapid-integration",
-    title: "Rapid feature integration",
-    icon: require('~/assets/icons/features-integration.svg'),
-    link: "/features#rapid-integration"
-  },
-  {
-    id: "multi-language",
-    title: "Multi-language applications",
-    icon: require('~/assets/icons/multi-language.svg'),
-    link: "/features#multi-language"
-  },
-  {
-    id: "event-driven",
-    title: "Event-driven architecture",
-    icon: require('~/assets/icons/event-driven.svg'),
-    link: "/features#event-driven"
-  },
-  {
-    id: "autonomous-workflows",
-    title: "Autonomous workflows",
-    icon: require('~/assets/icons/workflows.svg'),
-    link: "/features#autonomous-workflows"
-  },
-  {
-    id: "hybrid-decentralized-applications",
-    title: "Decentralized applications",
-    icon: require('~/assets/icons/decentralized.svg'),
-    link: "/features#hybrid-decentralized-applications"
-  },
-  {
-    id: "scalable",
-    title: "Scalable applications",
-    icon: require('~/assets/icons/scalability.svg'),
-    link: "/features#scalable"
-  }
-]
+import { mapState } from 'vuex'
+import MiniCard from '~/components/Cards/Mini'
+
 export default {
-  computed: {
-    featuresicon () {
-      return featuresicon
-    }
-  }
+  components: {
+    MiniCard
+  },
+
+  computed: mapState([ 'dotFeatures' ])
 }
 </script>
 
 <style scoped>
-
 h2 {
   text-align: center;
 }
-
-p {
-  text-decoration: none;
-  text-align:center;
-}
-
-svg{
-  margin-top:0;
-}
-
-.icon {
-  max-width: 50px;
-}
-
-.entry {
-  border-radius:0.3em;
-  text-decoration: none;
-  width: 200px;
-  height: 170px;
-  padding:1.8em;
-}
-.entry:hover {
-  position:relative;
-  transition: calc(var(--animation-speed) * 0.1s) ease;
-  background-color: #ffffff;
-  background-color: var(--white-content);
-  box-shadow: 0 0 40px 2px rgba(0, 0, 0, 0.07);
-  cursor: pointer;
-}
-
-@media only screen and (max-width: 414px) {
-  .entry {
-    width: 100%;
-    height: auto;
-  }
-}
-
 </style>
