27a251edb04e034913b4d4390b5f5f2f84608b13
Anthony ESTEBE <anthony@mesg.com>
2019-05-08 15:17:52 +0700

    add marketplace button

--- pages/marketplace.vue
+++ pages/marketplace.vue
@@ -1,32 +1,40 @@
 <template>
   <div>
-      <Header id="introduction" class="dark"
-        :title="title"
-        :description="description"
-        :schema="schema" />
-      <MarketStory id="market story" class="white"/>
-      <CTAGetStarted id="get started" class="dark"/>
+    <Header
+      id="introduction"
+      class="dark"
+      :title="title"
+      :description="description"
+      :schema="schema"
+      actionLink="https://marketplace.mesg.com"
+      actionTitle="Checkout the MESG Marketplace"
+    />
+    <MarketStory id="market story" class="white"/>
+    <CTAGetStarted id="get started" class="dark"/>
   </div>
 </template>
 
 <script>
-import Header from '~/components/Header'
-import SchemaMarketplace from '~/components/schema/Marketplace'
-import MarketStory from '~/components/MarketStory'
-import CTAGetStarted from '~/components/cta/GetStarted'
-import page from './page'
+import Header from "~/components/Header";
+import SchemaMarketplace from "~/components/schema/Marketplace";
+import MarketStory from "~/components/MarketStory";
+import CTAGetStarted from "~/components/cta/GetStarted";
+import Button from "~/components/Button";
+import page from "./page";
 export default {
   components: {
     Header,
     MarketStory,
-    CTAGetStarted
+    CTAGetStarted,
+    Button
   },
   mixins: [
     page({
-      title: 'MESG Marketplace',
-      description: 'The new economy for buying and selling modular application components. Choose new application features, or monetize off the ones you create.',
+      title: "MESG Marketplace",
+      description:
+        "The new economy for buying and selling modular application components. Choose new application features, or monetize off the ones you create.",
       schema: SchemaMarketplace
     })
   ]
-}
+};
 </script>
