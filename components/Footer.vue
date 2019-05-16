81071915e7dff6b6c3716f3ce9c2d3d053fbaa01
Emmanuel Drouin <emmanuel@mesg.tech>
2019-05-16 14:54:54 +0700

    Add footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -6,42 +6,68 @@
           <div flex column one-quarter>
             <nuxt-link :to="links.home">
               <img src="~/assets/MESG-logo-horizontal-white.svg" alt="MESG" mb1>
-              <p class="copyright" mb2>© 2019 MESG Foundation, All rights reserved.</p>
-              <div flex row space-between class="icon">
-                <i class="fab fa-medium"></i>
-                <i class="fab fa-github"></i>
-                <i class="fas fa-comments"></i>
-                <i class="fab fa-discord"></i>
-                <i class="fab fa-twitter"></i>
-                <i class="fab fa-linkedin-in"></i>
-                <i class="fab fa-facebook-f"></i>
-              </div>
             </nuxt-link>
+            <p class="copyright" mb2>© 2019 MESG Foundation, All rights reserved.</p>
+            <div flex space-between>
+              <a href="https://medium.com/mesg" target="_blank" class="icon">
+                <i class="fab fa-medium"></i>
+              </a>
+              <a href="https://github.com/mesg-foundation" target="_blank" class="icon">
+                <i class="fab fa-github"></i>
+              </a>
+              <a href="https://forum.mesg.com/" target="_blank" class="icon">
+                <i class="fas fa-comments"></i>
+              </a>
+              <a href="https://discordapp.com/invite/SaZ5HcE" target="_blank" class="icon">
+                <i class="fab fa-discord"></i>
+              </a>
+              <a href="https://twitter.com/mesgfoundation" target="_blank" class="icon">
+                <i class="fab fa-twitter"></i>
+              </a>
+              <a href="https://www.linkedin.com/company/mesg/" target="_blank" class="icon">
+                <i class="fab fa-linkedin-in"></i>
+              </a>
+              <a href="https://www.facebook.com/mesgfoundation/" target="_blank" class="icon">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+            </div>
           </div>
           <div flex row three-quarter>
             <div flex column one-quarter>
               <p class="category" mb1>Products</p>
               <Button :to="links.engine" class="link" mb1>MESG Engine</Button>
               <Button :to="links.marketplace" class="link" mb1>MESG Marketplace</Button>
-              <Button :to="links.showcase" class="link" mb1>Showcase</Button>
+              <Button class="link" mb1>
+                <span>Showcase</span>
+              </Button>
               <Button :to="links.faq" class="link" mb1>FAQ</Button>
             </div>
             <div flex column one-quarter>
               <p class="category" mb1>Developers</p>
-              <Button :to="links.engine" class="link" mb1>Get Started</Button>
-              <Button :to="links.marketplace" class="link" mb1>Marketplace</Button>
-              <Button :to="links.showcase" class="link" mb1>Documentation</Button>
-              <Button :to="links.faq" class="link" mb1>Forum</Button>
+              <Button
+                href="https://docs.mesg.com/guide/quick-start-guide.html"
+                target="_blank"
+                class="link"
+                mb1
+              >Get started</Button>
+              <Button
+                href="https://marketplace.mesg.com/"
+                target="_blank"
+                class="link"
+                mb1
+              >Marketplace</Button>
+              <Button href="https://docs.mesg.com/" target="_blank" class="link" mb1>Documentation</Button>
+              <Button href="https://forum.mesg.com/" target="_blank" class="link" mb1>Forum</Button>
             </div>
             <div flex column one-quarter>
               <p class="category" mb1>Foundation</p>
-              <Button :to="links.engine" class="link" mb1>Overview</Button>
-              <Button :to="links.marketplace" class="link" mb1>Partners</Button>
-              <Button :to="links.showcase" class="link" mb1>Roadmap</Button>
-              <Button :to="links.faq" class="link" mb1>Blog</Button>
+              <Button :to="links.foundation" class="link" mb1>Overview</Button>
+              <Button :to="links.partners" class="link" mb1>Partners</Button>
+              <Button :to="links.roadmap" class="link" mb1>Roadmap</Button>
+              <Button href="https://medium.com/mesg" target="_blank" class="link" mb1>Blog</Button>
             </div>
             <div flex column one-quarter>
-              <Button :to="links.engine" class="link" mb1>Token</Button>
+              <Button :to="links.token" class="link" mb1>Token</Button>
             </div>
           </div>
         </nav>
@@ -51,9 +77,15 @@
 
       <section id="newsletter">
         <Container>
-          <div flex row space-between>
-            <p class="category text-center">Sign up for our monthly newsletter</p>
-          </div>
+          <form flex row align-center>
+            <p class="category text-right" mr2>Sign up for our monthly newsletter</p>
+            <div class="newsletter" flex row space-between>
+              <input type="text" name="email" value="Your email adress">
+              <Button class="link submit" type="submit">
+                <i class="fas fa-arrow-right"></i>
+              </Button>
+            </div>
+          </form>
         </Container>
       </section>
     </div>
@@ -102,12 +134,16 @@ export default {
   color: var(--light-purple);
 }
 
+span {
+  color: var(--light-purple);
+}
+
 i {
   margin-right: 0;
 }
 
 .icon {
-  font-size: 20px;
+  font-size: 18px;
   font-weight: normal;
   font-style: normal;
   font-stretch: normal;
@@ -140,5 +176,34 @@ img {
 nav {
   width: 100%;
   background-color: var(--dark-purple);
+}
+
+.text-right {
+  text-align: right;
+}
+
+.newsletter {
+  width: 300px;
+  padding: 20px;
+  border: solid 2px var(--light-purple);
+  border-radius: 3px;
+  background-color: var(--dark-purple);
+  color: var(--white);
+}
+
+form {
+  width: 800px;
+  text-align: center;
+}
+
+input[type="text"] {
+  font-size: 15px;
+  color: var(--white);
+  background-color: var(--dark-purple);
+  border: none;
+}
+
+.submit {
+  text-align: right;
 }
 </style>
