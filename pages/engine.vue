cfc61af6226b24a8f23c1e2d5d0268bd9d500213
Anthony ESTEBE <anthony@mesg.com>
2019-05-17 17:21:08 +0700

    force third columns

--- pages/engine.vue
+++ pages/engine.vue
@@ -24,6 +24,7 @@
           :src="feature.src"
           :title="feature.title"
           :text="feature.description"
+          third
         />
       </Container>
     </section>
--- pages/marketplace.vue
+++ pages/marketplace.vue
@@ -20,6 +20,7 @@
           :src="feature.src"
           :title="feature.title"
           :text="feature.description"
+          third
         />
       </Container>
     </section>
