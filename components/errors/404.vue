cf16134d3229cf4dbb1c1afec8d65ce0841b0567
Anthony ESTEBE <anthony@mesg.com>
2019-06-11 14:24:16 +0700

    update management of error and 404

---
+++ components/errors/404.vue
@@ -0,0 +1,39 @@
+<template>
+  <Header :picture="require('~/assets/404.svg')" :title="title" :description="description">
+    <div>
+      <Button :to="links.home" primary>Return to Homepage</Button>
+      <span spacer></span>
+    </div>
+  </Header>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import Header from "~/components/Header";
+import Button from "~/components/Button";
+export default {
+  components: {
+    Header,
+    Button
+  },
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    }
+  },
+  computed: mapGetters({
+    links: "links"
+  })
+};
+</script>
+
+<style scoped>
+header::before {
+  transform: none !important;
+}
+</style>
---
+++ layouts/error.vue
@@ -0,0 +1,24 @@
+<template>
+  <Error404 :title="title" :description="description"/>
+</template>
+
+<script>
+import Error404 from "~/components/errors/404";
+import page from "~/pages/page";
+
+export default {
+  components: {
+    Error404
+  },
+  mixins: [
+    page({
+      title: "Oops! Looks like something went wrong",
+      description:
+        "Our best men are taking care of it. Meanwhile click on the button below to go back to Home, let's act like nothing happened."
+    })
+  ],
+  props: ["error"]
+};
+</script>
+
+<style scoped>
