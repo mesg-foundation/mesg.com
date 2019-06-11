3ed1e3c0b4e2898e089465984db2ae6205c41849
Emmanuel Drouin <emmanuel@mesg.tech>
2019-06-11 11:18:29 +0700

    Add 404 page

---
+++ pages/_.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <Header :picture="require('~/assets/404.svg')" :title="title" :description="description">
+      <div>
+        <Button :to="links.home" primary>Return to Homepage</Button>
+        <span spacer></span>
+      </div>
+    </Header>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from "vuex";
+import Header from "~/components/Header";
+import Container from "~/components/Container";
+import Button from "~/components/Button";
+import GetStarted from "~/components/GetStarted";
+import page from "./page";
+
+export default {
+  components: {
+    Header,
+    Container,
+    Button,
+    GetStarted
+  },
+  mixins: [
+    page({
+      title: "Oops! Looks like something went wrong",
+      description:
+        "Our best men are taking care of it. Meanwhile click on the button below to go back to Home, let's act like nothing happened."
+    })
+  ],
+  computed: mapGetters({
+    links: "links"
+  })
+};
+</script>
+
+<style scoped>
+header::before {
+  transform: none !important;
+}
+</style>
