36f10368539d64e1d75e03ce79846b50c087cdf7
Will Morgan <38283291+w1l1ams@users.noreply.github.com>
2019-07-15 12:10:38 +0700

    Update index.vue

--- pages/showcase/index.vue
+++ pages/showcase/index.vue
@@ -67,7 +67,7 @@ export default {
     page({
       title: "Showcase",
       description:
-        "Not sure what to build? Get inspired by some examples showing the wide range of possibilities offered."
+        "Not sure what to build? Get inspired by examples showing the wide range of possibilities."
     })
   ],
   props: {
