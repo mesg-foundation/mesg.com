693454b93ec3dc30b12c9c23df19ca6ed759168f
Emmanuel Drouin <emmanuel@mesg.tech>
2020-01-02 16:01:13 +0100

    Footer hover update

--- components/Footer.vue
+++ components/Footer.vue
@@ -127,7 +127,7 @@ nav {
     color: $text-color;
     transition: 0.1s ease-in;
     &:hover {
-      color: $primary;
+      color: $title-color;
       font-weight: normal;
     }
   }
@@ -166,7 +166,7 @@ ul {
 }
 
 a:hover {
-  color: $primary;
+  color: $title-color;
   font-weight: bold;
 }
 
